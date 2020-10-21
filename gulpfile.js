9da893c7f5fad9cf9682bee2518933a74b68f062
asifo1 <a.shahriar1999@gmail.com>
2020-10-21 15:32:43 +0600

    minify js

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,8 @@ const { task, src, dest, series, watch } = gulp;
 const sass = require("gulp-sass");
 const cleanCSS = require("gulp-clean-css");
 const htmlmin = require('gulp-htmlmin');
+const uglify = require("gulp-uglify-es").default;
+const rename = require("gulp-rename");
 
 task("style", (done) => {
     src("assets/style.scss")
@@ -20,10 +22,20 @@ task('html', (done) => {
       done();
   });
 
+  task("minjs", (done) => {
+    src("assets/script.js")
+      .pipe(uglify())
+      .pipe(rename({ suffix: ".min" }))
+      .pipe(dest("assets"));
+    done();
+  });
+
   task("watch", function () {
     watch("assets/style.scss", series("style"));
+    watch("assets/script.js", series("minjs"));
     watch("src/index.html", series("html"));
   });
 
 
-task('default',series("style","html"));
+
+task('default',series("style","html","minjs"));
