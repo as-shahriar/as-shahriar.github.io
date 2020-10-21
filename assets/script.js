df4ef3cd24850f29cf1aa44e79ea0a9749a5b60e
asifo1 <a.shahriar1999@gmail.com>
2020-10-21 15:26:06 +0600

    Added theme and cookie

---
+++ assets/script.js
@@ -0,0 +1,62 @@
+
+
+    window.onload = ()=>{
+        theme = getCookie("theme")
+        if (theme)  document.body.setAttribute("data-theme",theme)
+        setTimeout(()=>{
+            document.getElementById("preloader").style.display = "none";
+            document.body.style.overflow = "auto";
+        },2000);
+    }
+
+    document.querySelectorAll(".copy").forEach(e=>{
+        e.addEventListener("click",()=>{
+        copy(e.textContent);
+    }) ;
+
+    });
+
+    copy = (value)=>{
+        dummy = document.createElement("input"),
+        document.body.appendChild(dummy);
+        dummy.value = value;
+        dummy.select();
+        document.execCommand("copy");
+        document.body.removeChild(dummy);
+    }
+
+    toggle_theme = ()=>{
+        if(document.body.getAttribute("data-theme")=="dark"){
+            document.body.setAttribute("data-theme","")
+            setCookie("theme","")
+        }
+        else{
+            document.body.setAttribute("data-theme","dark")
+            setCookie("theme","dark")
+        }
+    }
+
+
+    document.getElementById("checkbox").addEventListener("click",toggle_theme)
+
+
+
+    setCookie = (name,value)=>{
+        var date = new Date();
+        date.setTime(date.getTime() + (30*24*60*60*1000));
+        expires = "; expires=" + date.toUTCString(); 
+        document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+    }
+
+    getCookie = (name)=> {
+        var nameEQ = name + "=";
+        var ca = document.cookie.split(';');
+        for(var i=0;i < ca.length;i++) {
+            var c = ca[i];
+            while (c.charAt(0)==' ') c = c.substring(1,c.length);
+            if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+        }
+        return null;
+    }
+
+    
