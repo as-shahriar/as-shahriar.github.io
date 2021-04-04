09388eaf5ec3e008e69bb9f50ceb0bc0e3ed7c07
asifo1 <a.shahriar1999@gmail.com>
2021-04-04 12:29:15 +0600

    Minor changes in PWA

--- assets/script.js
+++ assets/script.js
@@ -56,7 +56,7 @@ if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
     navigator.serviceWorker
       .register("../sw_cached_site.js")
-      .then((reg) => console.log("Service Worker: Registered (Pages)"))
+      .then((reg) => {})
       .catch((err) => console.log(`Service Worker: Error: ${err}`));
   });
 }
