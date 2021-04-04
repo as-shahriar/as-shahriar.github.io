1fb8f649c248b01cbb9e116ae0c8818f669b148d
asifo1 <a.shahriar1999@gmail.com>
2021-04-04 12:25:45 +0600

    Added PWA

--- assets/script.js
+++ assets/script.js
@@ -1,62 +1,62 @@
+window.onload = () => {
+  theme = getCookie("theme");
+  if (theme) document.body.setAttribute("data-theme", theme);
+  setTimeout(() => {
+    document.getElementById("preloader").style.display = "none";
+    document.body.style.overflow = "auto";
+  }, 2000);
+};
 
+document.querySelectorAll(".copy").forEach((e) => {
+  e.addEventListener("click", () => {
+    copy(e.textContent);
+  });
+});
 
-    window.onload = ()=>{
-        theme = getCookie("theme")
-        if (theme)  document.body.setAttribute("data-theme",theme)
-        setTimeout(()=>{
-            document.getElementById("preloader").style.display = "none";
-            document.body.style.overflow = "auto";
-        },2000);
-    }
+copy = (value) => {
+  (dummy = document.createElement("input")), document.body.appendChild(dummy);
+  dummy.value = value;
+  dummy.select();
+  document.execCommand("copy");
+  document.body.removeChild(dummy);
+};
 
-    document.querySelectorAll(".copy").forEach(e=>{
-        e.addEventListener("click",()=>{
-        copy(e.textContent);
-    }) ;
+toggle_theme = () => {
+  if (document.body.getAttribute("data-theme") == "dark") {
+    document.body.setAttribute("data-theme", "");
+    setCookie("theme", "");
+  } else {
+    document.body.setAttribute("data-theme", "dark");
+    setCookie("theme", "dark");
+  }
+};
 
-    });
+document.getElementById("checkbox").addEventListener("click", toggle_theme);
 
-    copy = (value)=>{
-        dummy = document.createElement("input"),
-        document.body.appendChild(dummy);
-        dummy.value = value;
-        dummy.select();
-        document.execCommand("copy");
-        document.body.removeChild(dummy);
-    }
+setCookie = (name, value) => {
+  var date = new Date();
+  date.setTime(date.getTime() + 30 * 24 * 60 * 60 * 1000);
+  expires = "; expires=" + date.toUTCString();
+  document.cookie = name + "=" + (value || "") + expires + "; path=/";
+};
 
-    toggle_theme = ()=>{
-        if(document.body.getAttribute("data-theme")=="dark"){
-            document.body.setAttribute("data-theme","")
-            setCookie("theme","")
-        }
-        else{
-            document.body.setAttribute("data-theme","dark")
-            setCookie("theme","dark")
-        }
-    }
+getCookie = (name) => {
+  var nameEQ = name + "=";
+  var ca = document.cookie.split(";");
+  for (var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == " ") c = c.substring(1, c.length);
+    if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+  }
+  return null;
+};
 
-
-    document.getElementById("checkbox").addEventListener("click",toggle_theme)
-
-
-
-    setCookie = (name,value)=>{
-        var date = new Date();
-        date.setTime(date.getTime() + (30*24*60*60*1000));
-        expires = "; expires=" + date.toUTCString(); 
-        document.cookie = name + "=" + (value || "")  + expires + "; path=/";
-    }
-
-    getCookie = (name)=> {
-        var nameEQ = name + "=";
-        var ca = document.cookie.split(';');
-        for(var i=0;i < ca.length;i++) {
-            var c = ca[i];
-            while (c.charAt(0)==' ') c = c.substring(1,c.length);
-            if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
-        }
-        return null;
-    }
-
-    
+// Make sure sw are supported
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("../sw_cached_site.js")
+      .then((reg) => console.log("Service Worker: Registered (Pages)"))
+      .catch((err) => console.log(`Service Worker: Error: ${err}`));
+  });
+}
