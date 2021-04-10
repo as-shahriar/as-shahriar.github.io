fce57c595a1c6dd290cd9ee8b292748b6695929b
asifo1 <a.shahriar1999@gmail.com>
2021-04-10 14:59:38 +0600

    moved cockie to localstorage

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,5 @@
-window.onload = () => {
-  theme = getCookie("theme");
-  if (theme) document.body.setAttribute("data-theme", theme);
-};
+theme = localStorage.getItem("theme");
+if (theme) document.body.setAttribute("data-theme", theme);
 
 document.querySelectorAll(".copy").forEach((e) => {
   e.addEventListener("click", () => {
@@ -20,10 +18,10 @@ copy = (value) => {
 toggle_theme = () => {
   if (document.body.getAttribute("data-theme") == "dark") {
     document.body.setAttribute("data-theme", "");
-    setCookie("theme", "");
+    localStorage.setItem("theme", "");
   } else {
     document.body.setAttribute("data-theme", "dark");
-    setCookie("theme", "dark");
+    localStorage.setItem("theme", "dark");
   }
 };
 
@@ -34,17 +32,6 @@ setCookie = (name, value) => {
   date.setTime(date.getTime() + 30 * 24 * 60 * 60 * 1000);
   expires = "; expires=" + date.toUTCString();
   document.cookie = name + "=" + (value || "") + expires + "; path=/";
-};
-
-getCookie = (name) => {
-  var nameEQ = name + "=";
-  var ca = document.cookie.split(";");
-  for (var i = 0; i < ca.length; i++) {
-    var c = ca[i];
-    while (c.charAt(0) == " ") c = c.substring(1, c.length);
-    if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
-  }
-  return null;
 };
 
 // Make sure sw are supported
